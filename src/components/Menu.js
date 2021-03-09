f7d8ac31f8492ba903e3e74b3ce07a88536da907
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-09 16:34:07 -0300

    addition: added Menu component with the basic navbar styles and routing

---
+++ src/components/Menu.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Menu() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Inicio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/admin">
+            Admin
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
