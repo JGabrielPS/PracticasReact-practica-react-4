d3179d16da84621ef9d4e81d62b5d1d96d92d7e4
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-09 16:59:43 -0300

    addition: added all neccesary UI elements of Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,28 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="row mt-2">
+      <div className="col"></div>
+      <div className="col">
+        <form action="" className="form-group">
+          <input
+            type="email"
+            className="form-control mt-4"
+            placeholder="Introduce el email"
+          />
+          <input
+            type="password"
+            className="form-control mt-4"
+            placeholder="Introduce el password"
+          />
+          <input
+            type="submit"
+            className="btn btn-dark btn-block mt-4"
+            value="Registrar un usuario"
+          />
+        </form>
+      </div>
+      <div className="col"></div>
     </div>
   );
 }
