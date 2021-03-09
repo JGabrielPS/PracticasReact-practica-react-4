a306d70088249de8e972cae9af892098ce784fd1
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-09 16:36:23 -0300

    addition: added Inicio, Login, Admin and Menu components using react-router-dom components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Inicio from "./components/Inicio";
+import Admin from "./components/Admin";
+import Login from "./components/Login";
+import Menu from "./components/Menu";
+
 function App() {
   return (
-    <>
-      <h1>Hola!!!</h1>
-    </>
+    <div className="container">
+      <Router>
+        <Menu />
+        <Switch>
+          <Route exact path="/" component={Inicio}></Route>
+          <Route path="/admin" component={Admin}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
